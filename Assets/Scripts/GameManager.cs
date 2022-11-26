48120f8f156cbeceb72ccd86b32621daaa396ad1
Óscar <oscar.boullosa@estudiantat.upc.edu>
2022-11-26 23:21:43 +0100

    Video 5 terminado: Game Manager
    Video 5 terminado: Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    private int level = 3;
 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
     // Update is called once per frame
     void Update()
     {
