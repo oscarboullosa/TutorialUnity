81add3321403bed11794e2134d0d37a77b0d5280
Óscar <oscar.boullosa@estudiantat.upc.edu>
2022-11-27 10:57:17 +0100

    Video 11 terminado: "Enemy Animator Controller"
    Video 11 terminado: "Enemy Animator Controller"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
     public int playerFoodPoints = 100;
     [HideInInspector] public bool playersTurn = true;
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,11 +22,13 @@ public class GameManager : MonoBehaviour
         else if (instance != this)
             Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
     }
     public void GameOver()
@@ -32,6 +38,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playersTurn || enemiesMoving)
+            return;
+        StartCoroutine(MoveEnemies());
+    }
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
