f87a06ce1a881473042de1514da37bce9e2e388c
Óscar <oscar.boullosa@estudiantat.upc.edu>
2022-11-27 11:34:12 +0100

    Video 12 terminado:"UI & Levels"
    Video 12 terminado:"UI & Levels"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MovingObject
 {
@@ -8,6 +9,7 @@ public class Player : MovingObject
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
     public float restartLevelDelay = 1f;
+    public Text foodText;
     private Animator animator;
     private int food;
     // Start is called before the first frame update
@@ -15,6 +17,7 @@ public class Player : MovingObject
     {
         animator = GetComponent<Animator>();
         food = GameManager.instance.playerFoodPoints;
+        foodText.text = "Food: " + food;
         base.Start();
     }
     private void OnDisable()
@@ -38,6 +41,7 @@ public class Player : MovingObject
     protected override void AttemptMove <T>(int xDir,int yDir)
     {
         food--;
+        foodText.text = "Food: " + food;
         base.AttemptMove<T>(xDir, yDir);
         RaycastHit2D hit;
         CheckIfGameOver();
@@ -53,11 +57,13 @@ public class Player : MovingObject
         else if (other.tag == "Food")
         {
             food += pointsPerFood;
+            foodText.text = "+" + pointsPerFood + " Food: " + food;
             other.gameObject.SetActive(false);
         }
         else if (other.tag == "Soda")
         {
             food += pointsPerSoda;
+            foodText.text = "+" + pointsPerSoda + " Food: " + food;
             other.gameObject.SetActive(false);
         }
     }
@@ -75,6 +81,7 @@ public class Player : MovingObject
     {
         animator.SetTrigger("playerHit");
         food -= loss;
+        foodText.text = "-" + loss + " Food: " + food;
         CheckIfGameOver();
     }
     private void CheckIfGameOver()
